bbc5c1e6255b95f53b5499dbb8e7e999acfcd17e
ckardaras <ckardaras92@gmail.com>
2019-12-03 21:53:12 -0700

    bunch of queries for team page

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ sql+="CREATE TABLE IF NOT EXISTS `League`.`UserGroup` (`UserGroup_Id` INT NOT NU
 
 sql+="CREATE TABLE IF NOT EXISTS `League`.`User` (`User_Id` INT NOT NULL AUTO_INCREMENT,`username` VARCHAR(50) NOT NULL,`password` VARCHAR(50) NOT NULL,`Team_Id` INT NOT NULL,`UserGroup_Id` INT NULL,INDEX `TeamId_idx` (`Team_Id` ASC),UNIQUE INDEX `username_UNIQUE` (`username` ASC),PRIMARY KEY (`User_Id`),INDEX `User_Group_Id_idx` (`UserGroup_Id` ASC),CONSTRAINT `TeamId`FOREIGN KEY (`Team_Id`)REFERENCES `League`.`Team` (`Team_Id`)ON DELETE NO ACTION ON UPDATE NO ACTION,CONSTRAINT `User_Group_Id`FOREIGN KEY (`UserGroup_Id`)REFERENCES `League`.`UserGroup` (`UserGroup_Id`)ON DELETE NO ACTION ON UPDATE NO ACTION)ENGINE = InnoDB;" //make user table
 
-sql+="CREATE TABLE IF NOT EXISTS `League`.`Profile` (`User_Id` INT NOT NULL,`email` VARCHAR(100) NULL, `phone` VARCHAR(10) NULL,`ProfilePicLink` VARCHAR(100) NULL,PRIMARY KEY (`User_Id`),CONSTRAINT `User_Id`FOREIGN KEY (`User_Id`)REFERENCES `League`.`User` (`User_Id`)ON DELETE NO ACTION ON UPDATE NO ACTION)ENGINE = InnoDB;" //make profile table
+sql+="CREATE TABLE IF NOT EXISTS `League`.`Profile` (`User_Id` INT NOT NULL AUTO_INCREMENT,`email` VARCHAR(100) NULL,`phone` VARCHAR(10) NULL,`ProfilePicLink` VARCHAR(100) NULL, name VARCHAR(100) NULL,PRIMARY KEY (`User_Id`),CONSTRAINT `User_Id`FOREIGN KEY (`User_Id`)REFERENCES `League`.`User` (`User_Id`)ON DELETE NO ACTION ON UPDATE NO ACTION)ENGINE = InnoDB;" //make profile table
 
 sql+="CREATE TABLE IF NOT EXISTS `League`.`Match` (`Match_Id` INT NOT NULL AUTO_INCREMENT,`GameDate` DATE NULL,`Arena` VARCHAR(100) NULL,`Valid` VARCHAR(45) NOT NULL DEFAULT 'False',PRIMARY KEY (`Match_Id`))ENGINE = InnoDB;" //make match table
 
@@ -51,18 +51,30 @@ sql+="CREATE TABLE IF NOT EXISTS `League`.`Rules` (`Rule_Id` INT NOT NULL AUTO_I
 
 sql+="CREATE TABLE IF NOT EXISTS `League`.`GroupRules_Bridge` (`UserGroup_Id` INT NOT NULL,`Rule_Id` INT NOT NULL,PRIMARY KEY (`UserGroup_Id`, `Rule_Id`),INDEX `Rule_Id_idx` (`Rule_Id` ASC),CONSTRAINT `UserGroup_Id`FOREIGN KEY (`UserGroup_Id`)REFERENCES `League`.`UserGroup` (`UserGroup_Id`)ON DELETE NO ACTION ON UPDATE NO ACTION,CONSTRAINT `Rule_Id`FOREIGN KEY (`Rule_Id`)REFERENCES `League`.`Rules` (`Rule_Id`)ON DELETE NO ACTION ON UPDATE NO ACTION)ENGINE = InnoDB;" //make GroupRules_bridge table
 
-sql+="INSERT INTO User Values (1, 'admin','admin',0,1);"
 
 //sql+="SET SQL_MODE=@OLD_SQL_MODE;SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;" //set various variables back to default
 
 
 // Sql test code
 
-sql+="insert into User Values(2, 'Mario','pass',1, 2);"
-sql+="insert into User Values(3, 'Peach','pass',1, 2);"
-sql+="insert into User Values(4, 'Luigi','pass',1, 2);"
-sql+="insert into User Values(5, 'Yoshi','pass',1, 2);"
-sql+="insert into User Values(6, 'Bowser','pass',1, 3);"
+sql+="insert into User(username,password,Team_Id,UserGroup_Id) Values('Mario','pass',1, 2);"
+sql+="INSERT INTO Profile(email,phone,ProfilePicLink,name) Values('Mario@gmail.com','1234567891','../img/portrait/Mario.jpg','Mario Mario');"
+
+sql+="insert into User(username,password,Team_Id,UserGroup_Id) Values('Peach','pass',1, 2);"
+sql+="INSERT INTO Profile(email,phone,ProfilePicLink,name) Values('Princess@gmail.com','1237577891','../img/portrait/Peach.jpg','Princess Peach');"
+
+sql+="insert into User(username,password,Team_Id,UserGroup_Id) Values('Luigi','pass',1, 2);"
+sql+="INSERT INTO Profile(email,phone,ProfilePicLink,name) Values('Luigi@gmail.com','1234555591','../img/portrait/Luigi.jpg','Luigi Mario');"
+
+sql+="insert into User(username,password,Team_Id,UserGroup_Id) Values('Yoshi','pass',1, 2);"
+sql+="INSERT INTO Profile(email,phone,ProfilePicLink,name) Values('Yoshi@gmail.com','1234567891','../img/portrait/Yoshi.jpg','T. Yoshisaur');"
+
+sql+="insert into User(username,password,Team_Id,UserGroup_Id) Values('Bowser','pass',1, 1);"
+sql+="INSERT INTO Profile(email,phone,ProfilePicLink,name) Values('CoachBowser@gmail.com','5555555555','../img/portrait/Bowser.jpg','King Koopa');"
+
+sql+="insert into User(username,password,Team_Id,UserGroup_Id) Values('Somebody','password',2, 1);"
+sql+="INSERT INTO Profile(email,phone,ProfilePicLink,name) Values('Test@gmail.com','5555555555','../img/portrait/Bowser.jpg','Fred');"
+
 sql+="INSERT INTO Team Values(1,'Mario Party',1, 'I am Coach Bowser. These Are my Minions');"
 sql+="INSERT INTO Team Values(2,'Another Test Team',1, 'This is another Test Team');"
 
@@ -217,7 +229,7 @@ app.get('/register', function(req, res) {
 	});
 });
 
-app.post('/sign_up', function(req,res){
+app.post('/sign_up_p', function(req,res){
     var name = req.player_form.player_fullName;
     var username = req.player_form.player_userName;
     var email =req.player_form.player_emailAddress;
@@ -233,6 +245,7 @@ app.post('/sign_up', function(req,res){
         "passwordconfirm":pass2,
         "phone":phone
     }
+var query1="INSERT INTO User ()"
 connection.query('INSERT INTO User Values('+name+');',function(error, results, fields){
         if (err) throw err;
         console.log("Record inserted Successfully");
@@ -249,15 +262,27 @@ app.get('/team',function(req,res){
     {
         res.redirect('/login')
     }
-    var query1='Select * from User where Team_Id=1;';
-    var query2='Select * from Team where Team_Id=1;';
-    db.query(query1+query2, function(err, rows, fields) {
+    var user = req.session.name;
+    var query1="Select * from User Where Team_Id=(Select Team_Id from User where username IN('";
+    query1+=req.session.name;
+    query1+="'));";
+
+    
+    var query2="Select * from Team where Team_Id=(Select Team_Id from User where username IN('";
+    query2+=req.session.name;
+    query2+="'));";
+
+    var query3="Select p.name from User u inner join Profile p on u.User_Id=p.User_Id inner join Team t on t.Team_Id=u.Team_Id Where u.Team_Id=(Select Team_Id from User where username IN('";
+    query3+=req.session.name;
+    query3+="'));";
+    db.query(query1+query2+query3, function(err, rows, fields) {
     if(!err)
     {
         res.render('pages/team.pug',{
             css:'../css/teampage.css',
             data1:rows[0],
             data2:rows[1],
+            data3:rows[2],
             title:'Team Page'
         });
     }
