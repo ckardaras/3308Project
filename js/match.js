f7b5984cdf066d5ddeaaaed75ba0510d09ac6159
Oceane Andreis <oceanestars13@gmail.com>
2019-12-05 15:19:21 -0700

    You can add location and date to the matchfinder page

--- js/match.js
+++ js/match.js
@@ -1,10 +1,13 @@
 // https://www.codexworld.com/bootstrap-datetimepicker-add-date-time-picker-input-field/
+// https://developer.mozilla.org/en-US/docs/Web/API/Document/createElement
 
 function AddDateLocation() {
   var Loc = $("#loc").val();
   var list = document.getElementById("Thicklist");
   //var confirmMyInput = document.getElementById("datetime");
-  var s = $("#dateTime").val();
+  //var s = $("#dateTime_").val();
+
+  var s = document.getElementById("datetime_").value;
 //  var dateTime = new Date(s.replace(/-/g,'/').replace('T',' '));
   var button = document.getElementById("my_submit_button");
   var checker = 0;
@@ -18,4 +21,7 @@ function AddDateLocation() {
   var newLi2 = document.createElement("li");
   newLi2.innerHTML = s;
   document.body.appendChild(newLi2);
+  
+  var breaktag = document.createElement("br");
+  document.body.appendChild(breaktag);
   }
